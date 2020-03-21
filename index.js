9653ad7eac28a92fb629ea56843bce31a0a8c06d
maradelynie <32828701+maradelynie@users.noreply.github.com>
2020-03-20 21:15:50 -0300

    +cors
    testing

--- index.js
+++ index.js
@@ -12,7 +12,11 @@ const port = process.env.PORT || 3333; //porta padrão
 app.use(bodyParser.json());
 app.use('/', router);
 
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 
 
